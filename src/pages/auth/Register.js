8800ded8487f4346ee3878496d7463a9164256c6
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-04 22:18:15 +0200

    GP-8 - created register page

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import styles from './Auth.module.scss'
+import registerImg from './../../assets/register.jpg'
+import { Link } from 'react-router-dom'
+import Card from '../../components/card/Card'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <section className={`container ${styles.auth}`}>
+            <Card>
+                <div className={styles.form}>
+                    <h2>Register</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Confirm password"
+                            required
+                        />
+
+                        <button type="submit" className="--btn --btn-primary --btn-block">
+                            Register
+                        </button>
+                    </form>
+                    <span className={styles.register}>
+                        <p>
+                            Already have an account?
+                        </p>
+                        <Link to="/login">Login</Link>
+                    </span>
+                </div>
+            </Card>
+            <div className={styles.img}>
+                <img src={registerImg} alt="Register" width="400" />
+            </div>
+        </section>
     )
 }
 
