11b178c436c0b6f7039aa540c96bfd29740d2bfe
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-04 19:57:40 +0200

    GP-6 - created auth components&routes for them

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {Header, Footer} from './components';
-import {Home, Contact} from './pages';
+import { Header, Footer } from './components';
+import { Home, Contact, Login, Register, Reset } from './pages';
 
 function App() {
   return (
-      <>
-        <BrowserRouter>
-          <Header/>
-            <Routes>
-              <Route path='/' element={ <Home/> }/>
-              <Route path='/contact' element={ <Contact/> }/>
-            </Routes>
-          <Footer/>
-        </BrowserRouter>
-      </>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/reset' element={<Reset />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
