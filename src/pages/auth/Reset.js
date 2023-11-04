4c5ec5b1e65ff6e409a1c0c5bc175699a961254f
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-04 22:36:26 +0200

    GP9 - created reset page

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import styles from './Auth.module.scss'
+import resetImg from './../../assets/reset.jpg'
+import { Link } from 'react-router-dom'
+import Card from '../../components/card/Card'
 
 const Reset = () => {
     return (
-        <div>Reset</div>
+        <section className={`container ${styles.auth}`}>
+            <div className={styles.img}>
+                <img src={resetImg} alt="Reset password" width="400" />
+            </div>
+            <Card>
+                <div className={styles.form}>
+                    <h2>Reset password</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            required
+                        />
+                        <button type="submit" className="--btn --btn-primary --btn-block">
+                            Reset password
+                        </button>
+                        <div className={styles.links}>
+                            <p>
+                                <Link to="/login">Login</Link>
+                            </p>
+                            <p>
+                                <Link to="/register">Register</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </Card>
+        </section>
     )
 }
 
