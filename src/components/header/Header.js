eea0769a2d788d4c936978ff741b5104c70846e0
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-19 13:36:29 +0200

    GP-16 - monitor auth state

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
-import { FaShoppingCart } from 'react-icons/fa'
+import { FaShoppingCart, FaUserCircle } from 'react-icons/fa'
 import { HiOutlineMenuAlt3 } from 'react-icons/hi'
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "./../../firebase/config"
 import { toast } from 'react-toastify';
 
@@ -30,6 +30,26 @@ const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [displayName, setDisplayName] = useState("");
+
+  //Monitor currently sign in user
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user.displayName);
+        setDisplayName(user.displayName);
+        // if (user.displayName == null) {
+        //   const u1 = user.email.substring(0, user.email.indexOf("@"));
+        //   const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
+        //   setDisplayName(uName);
+        // } else {
+        //   setDisplayName(user.displayName);
+        // }
+      } else {
+        setDisplayName("");
+      }
+    });
+  }, []);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -84,6 +104,10 @@ const Header = () => {
           <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
               <NavLink to="/login" className={activeLink}>Login</NavLink>
+              <a href="#">
+                <FaUserCircle size={16}/>
+                Hi, {displayName}
+                </a>
               <NavLink to="/register" className={activeLink}>Register</NavLink>
               <NavLink to="/order-history" className={activeLink}>My Orders</NavLink>
               <NavLink to="/" onClick={logoutUser}>Logout</NavLink>
