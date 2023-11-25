dd57f550bdb2e187c39a54489831270a1b9a8e16
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-25 17:57:02 +0200

    GP-22 - designed navigation bar

--- src/components/admin/navbar/Navbar.js
+++ src/components/admin/navbar/Navbar.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUserName } from '../../../redux/slice/authSlice';
+import styles from "./Navbar.module.scss"
+import { GiCook } from 'react-icons/gi';
+import { NavLink } from 'react-router-dom';
+
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Navbar = () => {
+  const userName = useSelector(selectUserName);
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.navbar}>
+      <div className={styles.user}>
+        <GiCook size={150} color="#fff" />
+        <h4>{userName}</h4>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/admin/home" className={activeLink}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/all-products" className={activeLink}>
+              All Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/add-product" className={activeLink}>
+              Add Product
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/orders" className={activeLink}>
+              Orders
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar
