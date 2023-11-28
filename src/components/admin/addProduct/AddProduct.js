a7a0ac19610b071e1f8802cc79d29665af15894b
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-28 23:01:10 +0200

    GP-27 - implement upload products logic

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import Card from '../../card/Card';
 import styles from './AddProduct.module.scss'
-import { ref, uploadBytesResumable } from 'firebase/storage';
-import { storage } from '../../../firebase/config';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { db, storage } from '../../../firebase/config';
+import { toast } from 'react-toastify';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import Loader from '../../loader/Loader';
 
 const continents = [
   { id: 1, name: "Europa" },
@@ -11,16 +15,23 @@ const continents = [
   { id: 4, name: "Africa" },
 ];
 
+const initialState = {
+  name: "",
+  imageURL: "",
+  price: 0,
+  continent: "",
+  country: "",
+  desc: "",
+};
+
 const AddProduct = () => {
   const [product, setProduct] = useState({
-    name: "",
-    imageURL: "",
-    price: null,
-    continent: "",
-    country: "",
-    desc: "",
+    ...initialState
+  });
 
-  })
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -31,15 +42,54 @@ const AddProduct = () => {
     const file = e.target.files[0]
     const storageRef = ref(storage, `gourmet_images/${Date.now()}${file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress)
+      },
+      (error) => {
+        toast.error(error.message)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setProduct({ ...product, imageURL: downloadURL })
+          toast.success("Imaginea a fost încărcată cu succes!")
+        });
+      }
+    );
   };
 
   const addProduct = (e) => {
     e.preventDefault();
-    console.log(product);
+    // console.log(product);
+    setIsLoading(true);
+
+    try {
+      const docRef = addDoc(collection(db, "products"), {
+        name: product.name,
+        imageURL: product.imageURL,
+        price: Number(product.price),
+        continent: product.continent,
+        country: product.country,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setIsLoading(false);
+      setUploadProgress(0);
+      setProduct({ ...initialState });
+
+      toast.success("Preparat încărcat cu succes!");
+      navigate("/admin/all-products");
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
   };
 
   return (
     <>
+      {isLoading && <Loader />}
       <div className={styles.product}>
         <h2>Adaugă un nou preparat</h2>
         <Card cardClass={styles.card}>
@@ -56,14 +106,18 @@ const AddProduct = () => {
 
             <label>Imagine:</label>
             <Card cardClass={styles.group}>
-              <div className={styles.progress}>
-                <div
-                  className={styles["progress-bar"]}
-                  style={{ width: "50%" }}
-                >
-                  Uploading 50%
+              {uploadProgress === 0 ? null : (
+                <div className={styles.progress}>
+                  <div
+                    className={styles["progress-bar"]}
+                    style={{ width: `${uploadProgress}%` }}
+                  >
+                    {uploadProgress < 100
+                      ? `Uploading ${uploadProgress}`
+                      : `Upload Complete ${uploadProgress}%`}
+                  </div>
                 </div>
-              </div>
+              )}
 
               <input
                 type="file"
@@ -73,16 +127,16 @@ const AddProduct = () => {
                 onChange={(e) => handleImageChange(e)}
               />
 
-
-              <input
-                type="text"
-                // required
-                placeholder="Image URL"
-                name="imageURL"
-                value={product.imageURL}
-                disabled
-              />
-
+              {product.imageURL === "" ? null : (
+                <input
+                  type="text"
+                  // required
+                  placeholder="Image URL"
+                  name="imageURL"
+                  value={product.imageURL}
+                  disabled
+                />
+              )}
             </Card>
 
             <label>Preț:</label>
