20574744f196f07388ec1d2d8f909c8ee2ca5649
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-11-30 12:19:59 +0200

    GP-28 - get products from firestore

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from './ViewProducts.module.scss'
+import { toast } from 'react-toastify'
+import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../../../firebase/config'
 
 const ViewProducts = () => {
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = () => {
+    setIsLoading(true)
+
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+      });
+
+    } catch (error) {
+      setIsLoading(false)
+      toast.error(error.message)
+    }
+  };
+
   return (
     <div>ViewProducts</div>
   )
