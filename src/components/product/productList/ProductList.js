d0a33e3b477250643744e93fff48346698099402
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-12-08 21:31:19 +0200

    GP-34&GP-35 - implemented Search&ProductList comp

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from './ProductList.module.scss'
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import Search from '../../search/Search';
+import { useDispatch } from 'react-redux';
+import ProductItem from '../productItem/ProductItem';
 
-const ProductList = () => {
+const ProductList = ({ products }) => {
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("latest");
+
+  const dispatch = useDispatch();
+
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
 
-export default ProductList
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+
+          <p>
+            <b>10</b> preparate găsite
+          </p>
+        </div>
+        {/* Search Icon */}
+        <div>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+        {/* Sort Products */}
+        <div className={styles.sort}>
+          <label>Sortează după:</label>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="latest">Recente</option>
+            <option value="lowest-price">Preț crescător</option>
+            <option value="highest-price">Preț descrescător</option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
+      </div>
+
+      <div className={grid ? `${styles.grid}` : `${styles.list}`}>
+        {products.lenght === 0 ? (
+          <p>Nu a fost găsit niciun preparat.</p>
+        ) : (
+          <>
+            {products.map((product) => {
+              return (
+                <div key={product.id}>
+                  <ProductItem {...product} grid={grid} product={product} />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
