91b2dac5316ba24f494e3fa9556670c5da395d8a
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2023-12-28 19:38:30 +0200

    GP-90 - implemented search feature for filter products

--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from './ProductFilter.module.scss'
 
 const ProductFilter = () => {
   return (
-    <div>ProductFilter</div>
-  )
+    <div className={styles.filter}>
+      <h4>Continent</h4>
+      <div className={styles.continent}>
+            <button>All</button>
+      </div>
+      <h4>Țară de origine</h4>
+      <div className={styles.country}>
+        <select name="brand">
+              <option value = "all">
+                All
+              </option>
+        </select>
+        <h4>Preț</h4>
+        <p>1500</p>
+        <div className={styles.price}>
+          <input
+            type="range"
+            name="price"
+            min={100}
+            max={1000}
+          />
+        </div>
+        <br />
+        <button className="--btn --btn-danger">
+          Șterge filtrele
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductFilter
--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -4,11 +4,20 @@ import { BsFillGridFill } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import Search from '../../search/Search';
 import ProductItem from '../productItem/ProductItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { FILTER_BY_SEARCH, selectFilteredProducts } from '../../../redux/slice/filterSlice';
 
 const ProductList = ({ products }) => {
   const [grid, setGrid] = useState(true);
   const [search, setSearch] = useState("");
   const [sort, setSort] = useState("latest");
+  const filteredProducts = useSelector(selectFilteredProducts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FILTER_BY_SEARCH({ products, search }));
+  }, [dispatch, products, search]);
 
   return (
     <div className={styles["product-list"]} id="product">
@@ -48,7 +57,7 @@ const ProductList = ({ products }) => {
           <p>Nu a fost găsit niciun preparat.</p>
         ) : (
           <>
-            {products.map((product) => {
+            {filteredProducts.map((product) => {
               return (
                 <div key={product.id}>
                   <ProductItem {...product} grid={grid} product={product} />
