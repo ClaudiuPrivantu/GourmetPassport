587761953b44f495f0d3492dd23c7049f1608d38
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2024-01-06 22:28:41 +0200

    GP-103 - created Cart Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header, Footer, AdminOnlyRoute, ProductDetails } from './components';
-import { Home, Contact, Login, Register, Reset, Admin } from './pages';
+import { Home, Contact, Login, Register, Reset, Admin, Cart } from './pages';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -17,7 +17,8 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/reset' element={<Reset />} />
-          <Route path='/product-details/:id' element={<ProductDetails/>} />
+          <Route path='/product-details/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
 
           {/* the admin page is accessible only when the admin user is logged in */}
           <Route path='/admin/*' element={
--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,9 +1,119 @@
 import React from 'react'
+import styles from './Cart.module.scss'
+import { useSelector } from 'react-redux';
+import { selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '../../redux/slice/cartSlice';
+import { Link } from 'react-router-dom';
+import { FaTrashAlt } from 'react-icons/fa';
+import Card from '../../components/card/Card';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartItems = useSelector(selectCartItems);
+  const cartTotalAmount = useSelector(selectCartTotalAmount);
+  const cartTotalQuantity = useSelector(selectCartTotalQuantity);
 
-export default Cart
+  return (
+    <section>
+      <div className={`container ${styles.table}`}>
+        <h2>Coșul de cumpărături</h2>
+        {cartItems.length === 0 ? (
+          <>
+            <p>Coșul dumneavoastră este momentan gol!</p>
+            <br />
+            <div>
+              <Link to="/#products">&larr; Continuați cumpărăturile</Link>
+            </div>
+          </>
+        ) : (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th>Serie</th>
+                  <th>Preparat</th>
+                  <th>Preț</th>
+                  <th>Cantitate</th>
+                  <th>Total</th>
+                  <th>Acțiune</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((cart, index) => {
+                  const { id, name, price, imageURL, cartQuantity } = cart;
+                  return (
+                    <tr key={id}>
+                      <td>{index + 1}</td>
+                      <td>
+                        <p>
+                          <b>{name}</b>
+                        </p>
+                        <img
+                          src={imageURL}
+                          alt={name}
+                          style={{ width: "100px" }}
+                        />
+                      </td>
+                      <td>{price} LEI</td>
+                      <td>
+                        <div className={styles.count}>
+                          <button
+                            className="--btn"
+                          >
+                            -
+                          </button>
+                          <p>
+                            <b>{cartQuantity}</b>
+                          </p>
+                          <button
+                            className="--btn"
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                      <td>{(price * cartQuantity)} LEI</td>
+                      <td className={styles.icons}>
+                        <FaTrashAlt
+                          size={19}
+                          color="red"
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <div className={styles.summary}>
+              <button className="--btn --btn-danger"
+              >
+                Golește coșul de cumpărături
+              </button>
+              <div className={styles.checkout}>
+                <div>
+                  <Link to="/#products">&larr; Continuați cumpărăturile</Link>
+                </div>
+                <br />
+                <Card cardClass={styles.card}>
+                  <p>
+                    <b> {`Numărul de produse: ${cartTotalQuantity}`}</b>
+                  </p>
+                  <div className={styles.text}>
+                    <h4>Subtotal:</h4>
+                    <h3>{`${cartTotalAmount} LEI`}</h3>
+                  </div>
+                  <p>Taxa de livrare este afișată la checkout</p>
+                  <button
+                    className="--btn --btn-primary --btn-block"
+                  >
+                    Checkout
+                  </button>
+                </Card>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
