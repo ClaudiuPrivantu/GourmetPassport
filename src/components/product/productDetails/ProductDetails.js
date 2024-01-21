3818a1ebdae80fd20e86a5714e1ca3f4b6390ba7
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2024-01-21 21:03:43 +0200

    GP-60 - cleanup after factorization of fetchDoc

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,8 +1,5 @@
-import { doc, getDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
-import { db } from '../../../firebase/config';
-import { toast } from 'react-toastify';
 import styles from './ProductDetails.module.scss'
 import spinnerImg from './../../../assets/loader.gif'
 import { useDispatch, useSelector } from 'react-redux';
