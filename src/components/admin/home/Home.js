947e5d5f55981cca801c2f80fe9dfdf6d2d0b50b
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2024-01-27 19:24:05 +0200

    GP-64 - implemented InfoBox component

--- src/components/admin/home/Home.js
+++ src/components/admin/home/Home.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './Home.module.scss'
+import InfoBox from '../../infoBox/InfoBox';
+import { FaCartArrowDown } from 'react-icons/fa';
+import Chart from '../../chart/Chart';
+import { GiMoneyStack } from 'react-icons/gi';
+import { useDispatch, useSelector } from 'react-redux';
+import useFetchCollection from '../../../customHooks/useFetchCollection';
+import { STORE_PRODUCTS, selectProducts } from '../../../redux/slice/productSlice';
+import { CALCULATE_TOTAL_ORDER_AMOUNT, STORE_ORDERS, selectOrderHistory, selectTotalOrderAmount } from '../../../redux/slice/orderSlice';
+import { BiSolidBowlHot } from 'react-icons/bi';
+
+//Icons
+const earningIcon = <GiMoneyStack size={30} color="#b624ff" />;
+const productIcon = <BiSolidBowlHot size={30} color="#1f93ff" />;
+const ordersIcon = <FaCartArrowDown size={30} color="orangered" />;
 
 const Home = () => {
+  const products = useSelector(selectProducts);
+  const orders = useSelector(selectOrderHistory);
+  const totalOrderAmount = useSelector(selectTotalOrderAmount);
+
+  const fbProducts = useFetchCollection("products");
+  const { data } = useFetchCollection("orders");
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: fbProducts.data,
+      })
+    );
+
+    dispatch(STORE_ORDERS(data));
+
+    dispatch(CALCULATE_TOTAL_ORDER_AMOUNT());
+  }, [dispatch, data, fbProducts]);
+
   return (
-    <div>Home</div>
-  )
+    <div className={styles.home}>
+      <h2>Panou general</h2>
+      <div className={styles["info-box"]}>
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card1}`}
+          title={"Încasări"}
+          count={`${totalOrderAmount} LEI`}
+          icon={earningIcon}
+        />
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card2}`}
+          title={"Preparate"}
+          count={products.length}
+          icon={productIcon}
+        />
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card3}`}
+          title={"Comenzi"}
+          count={orders.length}
+          icon={ordersIcon}
+        />
+      </div>
+      <div>
+        <Chart />
+      </div>
+    </div>
+  );
 }
 
 export default Home
