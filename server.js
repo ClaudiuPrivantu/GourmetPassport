a9a7a8eba85735b1c0bb45017f81c2585cd44e4b
Privantu Claudiu <gabriel.privantu@student.upt.ro>
2024-02-24 14:01:01 +0200

    added builds

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    });
+  }
+
 app.get("/", (request, response) => {
     response.send("Într-un singur click, lumea gastronomiei vine la ușa voastră. Bine ați venit în lumea GourmetPassport!")
 })
